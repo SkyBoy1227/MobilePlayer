41461f7fab7d934e66351e79206206611ed5841d
ASUS <553728561@qq.com>
2018-02-01 16:52:45 +0800

    调用其他播放器&播放网络视频

---
+++ StartAllPlayer/src/main/java/com/sky/app/startallplayer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sky.app.startallplayer;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startPlayer(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse("http://192.168.56.1:8080/my_heart.mp4"), "video/*");
+        startActivity(intent);
+    }
+}
