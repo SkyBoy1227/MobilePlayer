d85dbc67a89c0feeac0a9fc285bb7b3b440c7ec4
ASUS <553728561@qq.com>
2018-02-07 18:02:34 +0800

    手机实现播放中央13电台

--- StartAllPlayer/src/main/java/com/sky/app/startallplayer/MainActivity.java
+++ StartAllPlayer/src/main/java/com/sky/app/startallplayer/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public void startPlayer(View view) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
 //        intent.setDataAndType(Uri.parse("http://192.168.56.1:8080/Video.mp4"), "video/*");
-        intent.setDataAndType(Uri.parse("http://192.168.56.1:8080/atguigu.wmv"), "video/*");
+//        intent.setDataAndType(Uri.parse("http://192.168.56.1:8080/atguigu.wmv"), "video/*");
+        intent.setDataAndType(Uri.parse("http://cctv13.live.cntv.dnion.com/cache/23_/seg0/index151799761-151799761.ts?AUTH=cntv000120170623UKcAP+4WA2X/ykRV7eMMQe1bC4aMFbw6QgkF54ZmGyEf7TxO0xXWHOCDUcBDlE2orel3gyBrbg/+Eiwbtzyq0g=="), "video/*");
 //        intent.setDataAndType(Uri.parse("http://vf2.mtime.cn/Video/2016/07/19/mp4/160719095812990469.mp4"), "video/*");
         startActivity(intent);
     }
