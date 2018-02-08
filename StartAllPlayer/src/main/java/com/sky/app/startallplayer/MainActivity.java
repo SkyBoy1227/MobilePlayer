6785becc248e31c9964ef5a3dafe65b17685afb5
ASUS <553728561@qq.com>
2018-02-08 14:54:21 +0800

    修改直播地址

--- StartAllPlayer/src/main/java/com/sky/app/startallplayer/MainActivity.java
+++ StartAllPlayer/src/main/java/com/sky/app/startallplayer/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW);
 //        intent.setDataAndType(Uri.parse("http://192.168.56.1:8080/Video.mp4"), "video/*");
 //        intent.setDataAndType(Uri.parse("http://192.168.56.1:8080/atguigu.wmv"), "video/*");
-        intent.setDataAndType(Uri.parse("http://cctv13.live.cntv.dnion.com/cache/23_/seg0/index151799761-151799761.ts?AUTH=cntv000120170623UKcAP+4WA2X/ykRV7eMMQe1bC4aMFbw6QgkF54ZmGyEf7TxO0xXWHOCDUcBDlE2orel3gyBrbg/+Eiwbtzyq0g=="), "video/*");
+        intent.setDataAndType(Uri.parse("http://cctv13.live.cntv.dnion.com/live/cctv13hls_/index.m3u8?ptype=1&amode=1&AUTH=cntv000120170623rXFOv/l23BTxLXn1MQM2blcE3gqrhRVy1ZIFfKN7yqPjmRlzbG4WIy1/WRiBKLdT0RDHkehz87NN5rwwqpHy9w=="), "video/*");
 //        intent.setDataAndType(Uri.parse("http://vf2.mtime.cn/Video/2016/07/19/mp4/160719095812990469.mp4"), "video/*");
         startActivity(intent);
     }
