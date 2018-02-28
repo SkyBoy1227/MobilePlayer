e9f8a38a7d6266a9b17f347a34933711a8277af7
ASUS <553728561@qq.com>
2018-02-28 16:03:26 +0800

    适配器代码重构

--- app/src/main/java/com/sky/app/mobileplayer/adapter/NetAudioPagerAdapter.java
+++ app/src/main/java/com/sky/app/mobileplayer/adapter/NetAudioPagerAdapter.java
@@ -116,70 +116,9 @@ public class NetAudioPagerAdapter extends BaseAdapter {
         if (convertView == null) {
             viewHolder = new ViewHolder();
             // 初始化
-            switch (itemViewType) {
-                case TYPE_VIDEO:
-                    // 视频
-                    convertView = View.inflate(context, R.layout.all_video_item, null);
-                    //在这里实例化特有的
-                    viewHolder.tv_play_nums = convertView.findViewById(R.id.tv_play_nums);
-                    viewHolder.tv_video_duration = convertView.findViewById(R.id.tv_video_duration);
-                    viewHolder.iv_commant = convertView.findViewById(R.id.iv_commant);
-                    viewHolder.tv_commant_context = convertView.findViewById(R.id.tv_commant_context);
-                    viewHolder.jcv_videoplayer = convertView.findViewById(R.id.jcv_videoplayer);
-                    break;
-                case TYPE_IMAGE:
-                    // 图片
-                    convertView = View.inflate(context, R.layout.all_image_item, null);
-                    viewHolder.iv_image_icon = convertView.findViewById(R.id.iv_image_icon);
-                    break;
-                case TYPE_TEXT:
-                    // 文字
-                    convertView = View.inflate(context, R.layout.all_text_item, null);
-                    break;
-                case TYPE_GIF:
-                    // gif
-                    convertView = View.inflate(context, R.layout.all_gif_item, null);
-                    viewHolder.iv_image_gif = convertView.findViewById(R.id.iv_image_gif);
-                    break;
-                case TYPE_AD:
-                    // 软件广告
-                    convertView = View.inflate(context, R.layout.all_ad_item, null);
-                    viewHolder.btn_install = convertView.findViewById(R.id.btn_install);
-                    viewHolder.iv_image_icon = convertView.findViewById(R.id.iv_image_icon);
-                    break;
-                default:
-                    break;
-            }
+            convertView = initView(convertView, viewHolder, itemViewType);
             // 初始化公共的视图
-            switch (itemViewType) {
-                case TYPE_VIDEO:
-                    // 视频
-                case TYPE_IMAGE:
-                    // 图片
-                case TYPE_TEXT:
-                    // 文字
-                case TYPE_GIF:
-                    // gif
-                    //加载除开广告部分的公共部分视图
-                    //user info
-                    viewHolder.iv_headpic = convertView.findViewById(R.id.iv_headpic);
-                    viewHolder.tv_name = convertView.findViewById(R.id.tv_name);
-                    viewHolder.tv_time_refresh = convertView.findViewById(R.id.tv_time_refresh);
-                    viewHolder.iv_right_more = convertView.findViewById(R.id.iv_right_more);
-                    //bottom
-                    viewHolder.iv_video_kind = convertView.findViewById(R.id.iv_video_kind);
-                    viewHolder.tv_video_kind_text = convertView.findViewById(R.id.tv_video_kind_text);
-                    viewHolder.tv_shenhe_ding_number = convertView.findViewById(R.id.tv_shenhe_ding_number);
-                    viewHolder.tv_shenhe_cai_number = convertView.findViewById(R.id.tv_shenhe_cai_number);
-                    viewHolder.tv_posts_number = convertView.findViewById(R.id.tv_posts_number);
-                    viewHolder.ll_download = convertView.findViewById(R.id.ll_download);
-                    break;
-                default:
-                    break;
-            }
-
-            // 中间公共部分 -所有的都有
-            viewHolder.tv_context = convertView.findViewById(R.id.tv_context);
+            initCommonView(convertView, viewHolder, itemViewType);
             // 设置tag
             convertView.setTag(viewHolder);
         } else {
@@ -189,6 +128,11 @@ public class NetAudioPagerAdapter extends BaseAdapter {
         // 绑定数据
         // 根据位置得到数据,绑定数据
         NetAudioPagerData.ListBean mediaItem = datas.get(position);
+        bindData(viewHolder, itemViewType, mediaItem);
+        return convertView;
+    }
+
+    private void bindData(ViewHolder viewHolder, int itemViewType, NetAudioPagerData.ListBean mediaItem) {
         switch (itemViewType) {
             case TYPE_VIDEO:
                 // 视频
@@ -244,8 +188,77 @@ public class NetAudioPagerAdapter extends BaseAdapter {
         }
 
 
-        //设置文本
+        // 设置文本
         viewHolder.tv_context.setText(mediaItem.getText());
+    }
+
+    private void initCommonView(View convertView, ViewHolder viewHolder, int itemViewType) {
+        switch (itemViewType) {
+            case TYPE_VIDEO:
+                // 视频
+            case TYPE_IMAGE:
+                // 图片
+            case TYPE_TEXT:
+                // 文字
+            case TYPE_GIF:
+                // gif
+                //加载除开广告部分的公共部分视图
+                //user info
+                viewHolder.iv_headpic = convertView.findViewById(R.id.iv_headpic);
+                viewHolder.tv_name = convertView.findViewById(R.id.tv_name);
+                viewHolder.tv_time_refresh = convertView.findViewById(R.id.tv_time_refresh);
+                viewHolder.iv_right_more = convertView.findViewById(R.id.iv_right_more);
+                //bottom
+                viewHolder.iv_video_kind = convertView.findViewById(R.id.iv_video_kind);
+                viewHolder.tv_video_kind_text = convertView.findViewById(R.id.tv_video_kind_text);
+                viewHolder.tv_shenhe_ding_number = convertView.findViewById(R.id.tv_shenhe_ding_number);
+                viewHolder.tv_shenhe_cai_number = convertView.findViewById(R.id.tv_shenhe_cai_number);
+                viewHolder.tv_posts_number = convertView.findViewById(R.id.tv_posts_number);
+                viewHolder.ll_download = convertView.findViewById(R.id.ll_download);
+                break;
+            default:
+                break;
+        }
+
+        // 中间公共部分 -所有的都有
+        viewHolder.tv_context = convertView.findViewById(R.id.tv_context);
+    }
+
+    private View initView(View convertView, ViewHolder viewHolder, int itemViewType) {
+        switch (itemViewType) {
+            case TYPE_VIDEO:
+                // 视频
+                convertView = View.inflate(context, R.layout.all_video_item, null);
+                //在这里实例化特有的
+                viewHolder.tv_play_nums = convertView.findViewById(R.id.tv_play_nums);
+                viewHolder.tv_video_duration = convertView.findViewById(R.id.tv_video_duration);
+                viewHolder.iv_commant = convertView.findViewById(R.id.iv_commant);
+                viewHolder.tv_commant_context = convertView.findViewById(R.id.tv_commant_context);
+                viewHolder.jcv_videoplayer = convertView.findViewById(R.id.jcv_videoplayer);
+                break;
+            case TYPE_IMAGE:
+                // 图片
+                convertView = View.inflate(context, R.layout.all_image_item, null);
+                viewHolder.iv_image_icon = convertView.findViewById(R.id.iv_image_icon);
+                break;
+            case TYPE_TEXT:
+                // 文字
+                convertView = View.inflate(context, R.layout.all_text_item, null);
+                break;
+            case TYPE_GIF:
+                // gif
+                convertView = View.inflate(context, R.layout.all_gif_item, null);
+                viewHolder.iv_image_gif = convertView.findViewById(R.id.iv_image_gif);
+                break;
+            case TYPE_AD:
+                // 软件广告
+                convertView = View.inflate(context, R.layout.all_ad_item, null);
+                viewHolder.btn_install = convertView.findViewById(R.id.btn_install);
+                viewHolder.iv_image_icon = convertView.findViewById(R.id.iv_image_icon);
+                break;
+            default:
+                break;
+        }
         return convertView;
     }
 
